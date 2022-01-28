4ddc9882f0ea7c8f85f113f8586dbe16c0a54930
smeekas <smeetapatel008@gmail.com>
2022-01-28 16:44:55 +0530

    wip: functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+//? NUMBERS +DECIMALS
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const decimal = document.querySelector("#decimal");
+
+//?CLEAR INPUT
+const back = document.querySelector("#back");
+const clear = document.querySelector("#clear");
+
+const plusminus = document.querySelector("#plusminus");
+
+//?OPERATORS
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const mul = document.querySelector("#mul");
+const div = document.querySelector("#div");
+const equal = document.querySelector("#equal");
+
+//?FE
+const fe = document.querySelector("#fe");
+const input = document.querySelector("#input").children[0];
